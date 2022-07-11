51b5aececf6bff1130356eb149def67eb1bb349c
Frans AHW <franswinata6@gmail.com>
2022-07-11 09:56:33 +0700

    Add files via upload

---
+++ src/fire.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  // Copy and paste your Firebase SDK code here
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire
